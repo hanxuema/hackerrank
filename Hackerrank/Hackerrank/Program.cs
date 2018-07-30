2a297065d679be424456d43d2c5418cec58b5757
Xavier Xie <xieyuehao@gmail.com>
2018-07-30 22:30:39 +1000

    add rotleft

--- Hackerrank/Hackerrank/Program.cs
+++ Hackerrank/Hackerrank/Program.cs
@@ -6,12 +6,36 @@ using System.Threading.Tasks;
 
 namespace Hackerrank
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-         
+            Console.Write(rotLeft(new int[] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 }, 13));
             Console.ReadKey();
         }
+        public static int[] rotLeft(int[] a, int d)
+        {
+            if (d == a.Length)
+            {
+                return a;
+            }
+            //create new array with same length 
+            var newArr = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i + d > a.Length - 1)
+                {
+                    newArr[i] = a[i+d-a.Length];
+                }
+                else
+                {
+                    newArr[i] = a[i + d];
+                }
+            }
+
+            return newArr;
+
+        }
+         
     }
 }
---
+++ Hackerrank/HackerrankTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hackerrank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void rotLeftTestTest()
+        {
+            var result = Program.rotLeft(new int[] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 }, 13);
+
+            Assert.AreSame(result,
+            new int[] { 87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60 });
+        }
+    }
+}
