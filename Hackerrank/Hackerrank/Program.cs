51060773048820d89b368fc33b65beb87975ffec
Xavier Xie <hanxuema@hotmail.com>
2018-08-08 08:26:00 +1000

    add

--- Hackerrank/Hackerrank/Program.cs
+++ Hackerrank/Hackerrank/Program.cs
@@ -10,9 +10,36 @@ namespace Hackerrank
     {
         public static void Main(string[] args)
         {
-            Console.Write(rotLeft(new int[] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 }, 13));
+            var text = Console.ReadLine();
+
+          
             Console.ReadKey();
         }
+
+        public static int addOne(int value)
+        {
+            return value + 1;
+        }
+
+        public static void print(string text)
+        {
+            var value = 0;
+            for (int b = 0; b < 1; b++)
+            {
+                for (int a = 0; a < 2; a++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            value = value + 5;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(value);
+        }
+
         public static int[] rotLeft(int[] a, int d)
         {
             if (d == a.Length)
@@ -36,6 +63,22 @@ namespace Hackerrank
             return newArr;
         }
 
+        public static int Fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
         public static void checkMagazine(string[] magazine, string[] note)
         {
             var result = true;
@@ -57,7 +100,7 @@ namespace Hackerrank
             }
             Console.Write(result ? "Yes" : "No");
         }
-         
+
         public static Dictionary<string, int> ConvertStringArrayToDictionary(string[] array)
         {
             var noteDic = new Dictionary<string, int>();
@@ -75,5 +118,69 @@ namespace Hackerrank
             }
             return noteDic;
         }
+
+        // Complete the whatFlavors function below.
+        public static void whatFlavors(int[] cost, int money)
+        {
+            var output = new StringBuilder(); ;
+            var results = CalcWhatFlavors(cost, money);
+
+            foreach (var kvp in results)
+            {
+                output.AppendLine(string.Format("{0} {1}", kvp.Key, kvp.Value));
+            }
+
+            Console.Write(output.ToString());
+        }
+
+        public static Dictionary<int, int> CalcWhatFlavors(int[] cost, int money)
+        {
+            var result = new Dictionary<int, int>();
+            for (int i = 0; i < cost.Length; i++)
+            {
+                var gap = money - cost[i];
+                var foundIndex = CheckIfItemInArrayAndReturnIndex(cost, gap, i);
+                if (foundIndex >= 0 && i < foundIndex)
+                {
+                    if (!result.ContainsKey(i + 1))
+                    {
+                        result.Add(i + 1, foundIndex + 1);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static int CheckIfItemInArrayAndReturnIndex(int[] arr, int item, int currentIdx)
+        {
+            var result = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (item == arr[i] && i != currentIdx)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public static bool CheckIfItemInArray(int[] arr, int item)
+        {
+            var result = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (item == arr[i])
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+
     }
 }
--- Hackerrank/HackerrankTests/ProgramTests.cs
+++ Hackerrank/HackerrankTests/ProgramTests.cs
@@ -19,11 +19,75 @@ namespace Hackerrank.Tests
             Assert.IsTrue(result.SequenceEqual(new int[] { 77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77 }));
         }
 
+
+
         [TestMethod()]
-        public void checkMagazineTest()
+        public void FibonacciShouldReturn0IfInputIs0()
         {
-            Assert.Fail();
+            var result = Program.Fibonacci(0);
+            Assert.AreEqual(result, 0);
         }
- 
+
+        [TestMethod()]
+        public void FibonacciShouldReturn1IfInputIs1()
+        {
+            var result = Program.Fibonacci(1);
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod()]
+        public void FibonacciShouldReturn1IfInputIs2()
+        {
+            var result = Program.Fibonacci(2);
+            Assert.AreEqual(result, 1);
+        }
+        [TestMethod()]
+        public void FibonacciShouldReturn2IfInputIs3()
+        {
+            var result = Program.Fibonacci(3);
+            Assert.AreEqual(result, 2);
+        }
+        [TestMethod()]
+        public void FibonacciShouldReturn8IfInputIs6()
+        {
+            var result = Program.Fibonacci(6);
+            Assert.AreEqual(result, 8);
+        }
+
+        [TestMethod()]
+        public void CalcWhatFlavorsTest()
+        {
+            var result = Program.CalcWhatFlavors(new int[] { 1, 4, 5, 3, 2 }, 5);
+            var expect = new Dictionary<int, int>();
+            expect.Add(1, 2);
+            expect.Add(4, 5);
+            Assert.IsTrue(result.SequenceEqual(expect));
+        }
+
+        [TestMethod()]
+        public void CheckIfItemInArrayTest()
+        {
+            var result = Program.CheckIfItemInArray(new int[] { 1, 4, 5, 3, 2 }, 5);
+            Assert.IsTrue(result);
+
+            result = Program.CheckIfItemInArray(new int[] { 2, 2, 4, 3 }, 4);
+            Assert.IsTrue(result);
+
+            result = Program.CheckIfItemInArray(new int[] { 2, 2, 4, 3 }, 7);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void CheckIfItemInArrayAndReturnIndexTest()
+        {
+            var result = Program.CheckIfItemInArrayAndReturnIndex(new int[] { 1, 4, 5, 3, 2 }, 5, -1);
+            Assert.AreEqual(result, 2);
+
+            result = Program.CheckIfItemInArrayAndReturnIndex(new int[] { 2, 2, 4, 3 }, 4, -1);
+            Assert.AreEqual(result, 2);
+
+            result = Program.CheckIfItemInArrayAndReturnIndex(new int[] { 2, 2, 4, 3 }, 7, -2);
+            Assert.AreEqual(result, -1);
+        }
     }
 }
